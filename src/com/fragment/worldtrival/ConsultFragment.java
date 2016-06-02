d6e69e54ed3ab467149d26c262db5990d4e86e35
Administrator <Administrator@FateProject>
2016-06-02 18:27:16 +0800

    buce

--- src/com/fragment/worldtrival/ConsultFragment.java
+++ src/com/fragment/worldtrival/ConsultFragment.java
@@ -8,14 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 /**
- *×ÉÑ¯Fragment 
+ *
  *
  */
 public class ConsultFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.guide_consult, container, false);
+		View view = inflater.inflate(R.layout.tabs_fragment_consult, container, false);
 		return view;
 	}
 }
