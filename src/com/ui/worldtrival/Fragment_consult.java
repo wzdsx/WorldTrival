82d9bb21780be44ea4410ffb0f54592ca7f86a6e
Administrator <Administrator@FateProject>
2016-06-02 18:15:16 +0800

    扥

---
+++ src/com/ui/worldtrival/Fragment_consult.java
@@ -0,0 +1,21 @@
+package com.ui.worldtrival;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Fragment_consult extends FragmentActivity {
+	int a = 2;
+	//test
+	@Override
+	protected void onCreate(Bundle arg0) {
+		super.onCreate(arg0);
+		setContentView(R.layout.tabs_fragment_consult);
+		
+	}
+	
+
+
+	
+}
