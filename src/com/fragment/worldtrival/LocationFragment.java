6b0e569f7ba7ed9307b1eb5962d2122a40a1f4a7
Administrator <Administrator@FateProject>
2016-06-02 18:29:53 +0800

    herwc

--- src/com/fragment/worldtrival/LocationFragment.java
+++ src/com/fragment/worldtrival/LocationFragment.java
@@ -8,14 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 /**
- *µ±µØFragment 
+ *ï¿½ï¿½ï¿½ï¿½Fragment 
  *
  */
 public class LocationFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.guide_location, container, false);
+		View view = inflater.inflate(R.layout.tabs_fragment_local, container, false);
 		return view;
 	}
 }
